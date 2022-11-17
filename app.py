9d768c6502acd44be1ec60c0f750dec15810acd6
Polo-Wolo <polo.walczak@gmail.com>
2022-11-17 18:24:04 +0100

    Add Heroku deployment files

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
-import json
-import datetime
+from flask_frozen import Freezer
+import json, sys
 
 # create a data file
 data = {}
@@ -15,10 +15,14 @@ with open("static/src/json/config.json", 'r') as file:
 	config = json.load(file)
 
 app = Flask(__name__)
-	
+freezer = Freezer(app)
+
 @app.route("/")
 def home():
-	return render_template("base.html", utc_dt=datetime.datetime.utcnow(), **data, **config)
+	return render_template("base.html", **data, **config)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	if len(sys.argv) > 1 and sys.argv[1] == "build":
+		freezer.freeze()
+	else:
+		app.run(debug=True)
