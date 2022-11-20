49bf6e6aeb5655374bd4267bafe243017c12cc83
Polo-Wolo <polo.walczak@gmail.com>
2022-11-20 22:27:58 +0100

    Mobile responsive big improvements

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template
 from flask_frozen import Freezer
 import json, sys
+import socket
+
+# Use to know the server ip in the network
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+ip_addr = s.getsockname()[0]
+s.close()
 
 # create a data file
 data = {}
@@ -25,4 +32,4 @@ if __name__ == '__main__':
 	if len(sys.argv) > 1 and sys.argv[1] == "build":
 		freezer.freeze()
 	else:
-		app.run(debug=True, host="192.168.1.68" )
+		app.run(debug=True, host=ip_addr )
