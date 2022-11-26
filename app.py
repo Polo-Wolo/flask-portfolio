877d5d0e957d4d9b2526b129e7aa2e315645404e
Polo-Wolo <polo.walczak@gmail.com>
2022-11-26 21:51:32 +0100

    Adding python script to correct build paths

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 from flask_frozen import Freezer
 import json, sys
 import socket
+import pathlib
+
 
 online = 0
 
@@ -37,7 +39,22 @@ if __name__ == '__main__':
 	if len(sys.argv) > 1 and sys.argv[1] == "build":
 		freezer.freeze()
 
-		
+		cur_dir = str(pathlib.Path(__file__).parent.resolve())
+
+		# Read in the file
+		with open(cur_dir+'/build/index.html', 'r') as file :
+			filedata = file.read()
+
+		# Replace the target string
+		filedata = filedata.replace('href="/static', 'href="../static')
+		filedata = filedata.replace('src="../../static', 'src="../static')
+		filedata = filedata.replace('src="/static', 'src="../static')
+		filedata = filedata.replace('href="../../static', 'href="../static')
+
+
+		# Write the file out again
+		with open(cur_dir+'/build/index.html', 'w') as file:
+			file.write(filedata)
 
 	else:
 		app.run(debug=True, host=ip_addr )
