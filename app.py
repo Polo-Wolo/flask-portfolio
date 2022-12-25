b9fde841a079c9abe970be5241122e1471de62be
Polo-Wolo <polo.walczak@gmail.com>
2022-12-25 10:01:16 +0100

    clean app.py

--- app.py
+++ app.py
@@ -52,41 +52,6 @@ def changeRelativePath():
 	with open(cur_dir+'/build/index.html', 'w') as file:
 		file.write(filedata)
 
-def cleanFiles(rltv_src_dir, rltv_dst_dir):
-	# get current directory path
-	cur_dir = str(pathlib.Path(__file__).parent.resolve())
-
-	# Cleaning directory for gh host
-	for files in os.listdir(cur_dir):
-		path = os.path.join(cur_dir, files)
-		#print(path)
-		if(path!=(cur_dir+'/build') and path!=(cur_dir+'/README.md') and path!=(cur_dir+'/.gitignore') and path!=(cur_dir+'/.git')):
-			try:
-				shutil.rmtree(path)
-			except OSError:
-				os.remove(path)
-	
-	# move everyfiles from build to current directory
-	src = cur_dir+rltv_src_dir
-	dst = cur_dir+rltv_dst_dir
-	print("src : "+src)
-	print("dst : "+dst)
-	
-	# Iterate through the list of items
-	for item in os.listdir(src):
-		# Construct the source and destination file paths
-		source_item = os.path.join(src, item)
-		dest_item = os.path.join(dst, item)
-		
-		# Move the item from the source to the destination
-		shutil.move(source_item, dest_item)
-
-	#remove build folder
-	try:
-		shutil.rmtree(cur_dir+'/build')
-	except OSError:
-		os.remove(cur_dir+'/build')
-
 def renameDirectory(old_dir_name, new_dir_name):
 	# get current directory path
 	cur_dir = str(pathlib.Path(__file__).parent.resolve())
